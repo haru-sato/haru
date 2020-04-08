15cd4bd539eecfd59f3190de7c12a1749edf3372
haru-sato <seattle-sato@gmail.com>
2020-04-08 18:07:03 +0900

    add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+<Header>
+    <div id ="headerSection">
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+    </div>
+</Header>
+</template>
+
+<style>
+    #headerSection{
+        background: #F3F3F3;
+        width: 100%;
+        height: auto;
+    }
+
+</style>
+
+<script>
+export default {
+name: 'Header',
+props: {
+msg: String
+}
+}
+</script>
