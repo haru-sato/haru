dd71b6f79ecd1f816bbd0dbf2e5f25b7ad6a1ae1
haru-sato <seattle-sato@gmail.com>
2020-04-20 18:17:02 +0900

    【ポートフォリオ実装】20200420

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+// npm install --save axios vue-axios を実行してデータにアクセスできるようにする
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+import smoothScroll from 'vue-smoothscroll'
+Vue.use(smoothScroll)
